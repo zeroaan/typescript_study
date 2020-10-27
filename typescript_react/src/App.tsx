338d8cb82e40ba71fff327e6f7d21cca8edb22bf
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-10-27 17:16:19 +0900

    ts react

--- typescript_react/src/App.tsx
+++ typescript_react/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Counter from "./Counter";
 import Home from "./Home";
 
 const App = () => {
   return (
     <>
       <Home title="typescript" desc="ts is good" />
+      <Counter />
     </>
   );
 };
