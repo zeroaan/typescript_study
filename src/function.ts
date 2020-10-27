4f368286b124f2979dbe8b1c2c874aa873677273
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-10-27 10:00:48 +0900

    기초 연습

---
+++ src/function.ts
@@ -0,0 +1,11 @@
+import { setSourceMapRange } from "typescript";
+
+function sum(x: number, y: number): number {
+  return x + y;
+}
+sum(1, 2);
+
+function sumArray(numbers: number[]): number {
+  return numbers.reduce((acc, current) => acc + current, 0);
+}
+const total = sumArray([1, 2, 3, 4, 5]);
---
+++ src/variable.ts
@@ -0,0 +1,19 @@
+let count: number = 0;
+count += 1;
+// count = "문자열";
+
+const message: string = "hello world";
+
+const done: boolean = true;
+
+const numbers: number[] = [1, 2, 3];
+const messages: string[] = ["hello", "world"];
+
+// messages.push(1);
+
+let mightBeUndefined: string | undefined = undefined;
+let nullableNumber: number | null = null;
+
+let color: "red" | "orange" | "yellow" = "red";
+color = "yellow";
+// color = "green";
