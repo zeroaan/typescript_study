406994cd2ea807c884968b2387439f4c3749ad3e
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-10-27 10:13:20 +0900

    interface

---
+++ src/interface.ts
@@ -0,0 +1,59 @@
+interface Shape {
+  getArea(): number;
+}
+
+class Circle implements Shape {
+  constructor(public radius: number) {
+    this.radius = radius;
+  }
+
+  getArea() {
+    return this.radius * this.radius * Math.PI;
+  }
+}
+
+class Rectangle implements Shape {
+  constructor(private width: number, private height: number) {
+    this.width = width;
+    this.height = height;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
+}
+
+const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
+
+shapes.forEach((shape) => {
+  console.log(shape.getArea());
+});
+
+const circle = new Circle(5);
+const rectangle = new Rectangle(10, 5);
+
+console.log(circle.radius);
+// console.log(rectangle.width);
+
+// @@@@@@@@@@@@@@@@@@@@@@
+
+interface Person {
+  name: string;
+  age?: number;
+}
+interface Developer extends Person {
+  skills: string[];
+}
+
+const person: Person = {
+  name: "peter",
+  age: 20,
+};
+const expert: Developer = {
+  name: "mark",
+  skills: ["Javascript, Typescript"],
+};
+
+const people: Person[] = [person, expert];
+
+console.log(people);
