988de6ea8fcb9dad0cd8ca53684f967eea020fb5
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-10-30 16:01:57 +0900

    numberBaseball

--- typescript_tsreact/lecture/client.tsx
+++ typescript_tsreact/lecture/client.tsx
@@ -2,9 +2,9 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { hot } from "react-hot-loader/root"
 
-import WordRelay from "./WordRelay";
+import NumberBaseball from "./NumberBaseball";
 
-const Hot = hot(WordRelay); // HOC
+const Hot = hot(NumberBaseball); // HOC
 
 ReactDOM.render(<Hot />, document.querySelector("#root"))
 
--- typescript_tsreact/lecture/webpack.config.js
+++ typescript_tsreact/lecture/webpack.config.js
@@ -1,27 +1,28 @@
-const path = require("path")
-const webpack = require("webpack")
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    mode: "development", // production
-    devtool: "eval", // hidden-source-map
-    resolve: {
-        extensions: [".jsx", ".js", ".tsx", ".ts"]
-    },
+  mode: "development", // production
+  devtool: "eval", // hidden-source-map
+  resolve: {
+    extensions: [".jsx", ".js", ".tsx", ".ts"],
+  },
 
-    entry: {
-        app: "./client"
-    },
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            loader: "awesome-typescript-loader"
-        }]
-    },
-    plugins: [
-
+  entry: {
+    app: "./client",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+      },
     ],
-    output: {
-        filename: '[name].js',
-        path: path.join(__dirname, "dist")
-    }
-}
+  },
+  plugins: [],
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "dist"),
+    publicPath: "/dist",
+  },
+};
