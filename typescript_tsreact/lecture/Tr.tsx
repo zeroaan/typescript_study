4f53f984a6b6ffd2f283321746303719152413d1
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-11-03 15:41:04 +0900

    tictactoe 너무 어렵

---
+++ typescript_tsreact/lecture/Tr.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { Dispatch, useMemo } from "react";
+import Td from "./Td";
+
+interface Props {
+  rowData: string[];
+  rowIndex: number;
+  dispatch: Dispatch<any>;
+}
+const Tr: React.FC<Props> = ({ rowData, rowIndex, dispatch }) => {
+  return (
+    <>
+      <tr>
+        {Array(rowData.length)
+          .fill(null)
+          .map((td, i) => {
+            useMemo(
+              () => (
+                <Td
+                  key={i}
+                  dispatch={dispatch}
+                  rowIndex={rowIndex}
+                  cellIndex={i}
+                  cellData={rowData[i]}
+                >
+                  {""}
+                </Td>
+              ),
+              [rowData[i]]
+            );
+          })}
+      </tr>
+    </>
+  );
+};
+
+export default Tr;
