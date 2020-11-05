f46821e02336a982780a100a1b98b5f8e9a0cf2b
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-11-05 16:26:41 +0900

    mineSearch finish

---
+++ typescript_tsreact/8.지뢰찾기/Form.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { useState, useCallback, useContext, memo } from "react";
+import { TableContext } from "./MineSearch";
+import { startGame } from "./MineSearch";
+
+const Form = () => {
+  const [row, setRow] = useState(10);
+  const [cell, setCell] = useState(10);
+  const [mine, setMine] = useState(20);
+  const { dispatch } = useContext(TableContext);
+
+  const onchangeInput = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { value, name } = e.target;
+      if (name === "row") {
+        setRow(Number(value));
+      } else if (name === "cell") {
+        setCell(Number(value));
+      } else if (name === "mine") {
+        setMine(Number(value));
+      }
+    },
+    []
+  );
+  const onClickBtn = useCallback(() => {
+    dispatch(startGame(row, cell, mine));
+  }, [row, cell, mine]);
+
+  return (
+    <>
+      <div>
+        <input
+          type="number"
+          placeholder="세로"
+          name="row"
+          value={row}
+          onChange={onchangeInput}
+        />
+        <input
+          type="number"
+          placeholder="가로"
+          name="cell"
+          value={cell}
+          onChange={onchangeInput}
+        />
+        <input
+          type="number"
+          placeholder="지뢰"
+          name="mine"
+          value={mine}
+          onChange={onchangeInput}
+        />
+        <button onClick={onClickBtn}>시작</button>
+      </div>
+    </>
+  );
+};
+
+export default memo(Form);
---
+++ typescript_tsreact/8.지뢰찾기/Table.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { useContext, memo } from "react";
+import { TableContext } from "./MineSearch";
+import Tr from "./Tr";
+
+const Table = () => {
+  const { tableData } = useContext(TableContext);
+
+  return (
+    <>
+      <table>
+        <tbody>
+          {Array(tableData.length)
+            .fill(null)
+            .map((tr, i) => (
+              <Tr key={i} rowIndex={i} />
+            ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default memo(Table);
