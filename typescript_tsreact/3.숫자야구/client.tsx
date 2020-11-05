eacc98bc6f96d45caf83447977bde2a743aaa817
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-11-05 10:05:14 +0900

    package수정(hot-loader, webpack)

--- typescript_tsreact/3.숫자야구/client.tsx
+++ typescript_tsreact/3.숫자야구/client.tsx
@@ -1,11 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { hot } from "react-hot-loader/root"
 
 import NumberBaseball from "./NumberBaseball";
 
-const Hot = hot(NumberBaseball); // HOC
-
-ReactDOM.render(<Hot />, document.querySelector("#root"))
-
-
+ReactDOM.render(<Hot />, document.querySelector("#root"));
